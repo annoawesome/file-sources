91e2d958e2fcd408dd820d74fb7dc96e04cded3b
Seth Hoong <hoongseth@gmail.com>
2026-04-04 21:24:05 +0000

    Add file listing and search elements with styling

--- src/public/App.tsx
+++ src/public/App.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
+function FileList() {
+  return (
+    <div>
+      <h2>Results</h2>
+      <div>
+        <FileEntry name="File Name 1" />
+      </div>
+    </div>
+  );
+}
+
+function FileEntry({ name }: { name: string }) {
+  return (
+    <article className="file-entry">
+      <h3 className="text-2">{name}</h3>
+      <button type="button" className="button-2">
+        Download
+      </button>
+    </article>
+  );
+}
+
+function SearchBar() {
+  return (
+    <input
+      className="search-1"
+      type="search"
+      name="a"
+      id=""
+      placeholder="Big Buck Bunny"
+    />
+  );
+}
+
 export default function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="flex-center">
+      <SearchBar />
+      <FileList />
+    </div>
+  );
 }
