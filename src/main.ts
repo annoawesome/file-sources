0879bd05f286d20fa2877607f90af725d7266dc1
Seth Hoong <hoongseth@gmail.com>
2026-04-11 00:31:39 +0000

    Add cookie-parser and authentication middleware

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 import { apiRouter } from "./routers/apiRouter.js";
 import { generateTables } from "./db/db.js";
@@ -9,6 +10,7 @@ const port = 8080;
 // TODO: Possible race condition?
 generateTables();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.static("public"));
 
---
+++ src/middleware/authenticate.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+type AuthPayload = {
+  authPayload: jwt.JwtPayload;
+};
+
+export default function authenticate(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const cookies = req.cookies;
+  const token: string | undefined = cookies.session_token;
+
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.FSC_SECRET_KEY as string);
+
+    (req as Request & AuthPayload).authPayload = decoded as jwt.JwtPayload;
+    next();
+  } catch {
+    res.sendStatus(403);
+  }
+}
