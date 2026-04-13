de84ca6dce1901dc366d375caa5f560a0be65401
Seth Hoong <hoongseth@gmail.com>
2026-04-13 01:29:49 +0000

    Implement file upload functionality in uploadRouter

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-type AuthPayload = {
+export type AuthPayload = {
   authPayload: jwt.JwtPayload;
 };
 
--- src/routers/uploadRouter.ts
+++ src/routers/uploadRouter.ts
@@ -1,11 +1,30 @@
-import express from "express";
-import authenticate from "../middleware/authenticate.js";
+import express, { Request } from "express";
+import authenticate, { AuthPayload } from "../middleware/authenticate.js";
+import uploadFile from "../dao/uploadFile.js";
+
+type UploadFileBody = {
+  fileContents: string;
+  fileName: string;
+};
 
 export const uploadRouter = express.Router();
 
 uploadRouter.use(authenticate);
 
-uploadRouter.post("file", (req, res) => {
-  // Not implemented
-  res.sendStatus(501);
+uploadRouter.post("file", async (req, res) => {
+  // Authentication middleware was used, so this should exist
+  const authPayload = (req as Request & AuthPayload).authPayload;
+  const body: UploadFileBody = req.body;
+
+  const sqlRows = await uploadFile(
+    Buffer.from(body.fileContents, "base64"),
+    body.fileName,
+    authPayload.username,
+  );
+
+  if (sqlRows.length > 0) {
+    res.sendStatus(201);
+  } else {
+    res.sendStatus(500);
+  }
 });
