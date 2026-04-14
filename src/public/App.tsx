ca741c92ccb3ee4edbd07ad20e0d0225abfbea33
Seth Hoong <hoongseth@gmail.com>
2026-04-14 00:59:15 +0000

    Add FileUpload component and integrate it into the app's routing

--- src/public/App.tsx
+++ src/public/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import FileSearch from "./component/FileSearch";
 import UserRegistration from "./component/UserRegistration";
 import UserLogin from "./component/UserLogin";
+import FileUpload from "./component/FileUpload";
 
 function ResolvedPage({
   location,
@@ -20,13 +21,17 @@ function ResolvedPage({
     return UserRegistration(setLocation);
   } else if (location === "user-login") {
     return UserLogin(setLocation, setUsername);
+  } else if (location === "file-upload") {
+    return FileUpload();
   }
 
   return <h1>Failed to resolve</h1>;
 }
 
 export default function App() {
-  const [pageLocation, setPageLocation] = useState("files-search");
+  const startingLocation = "file-upload";
+
+  const [pageLocation, setPageLocation] = useState(startingLocation);
   const [username, setUsername] = useState<string>("");
 
   return (
---
+++ src/public/component/FileUpload.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function FileUpload() {
+  return (
+    <div className="flex-center user-register-flex-center">
+      <h2 className="text-center">Upload File</h2>
+      <form action="" className="form-1">
+        <input type="file" name="file" id="" />
+        <button type="submit" className="button-2">
+          Upload
+        </button>
+      </form>
+    </div>
+  );
+}
